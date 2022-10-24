9619fce68c66266580fdb54eb62d42157320b74a
JanaElfeky <112937584+JanaElfeky@users.noreply.github.com>
2022-10-24 21:13:07 +0200

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -12,28 +12,26 @@ using namespace std;
 
 LinkedList::LinkedList(){
     head=NULL; //initializing head pointer
+    mirror=NULL;
+    mirror2=NULL;
 }
 node* LinkedList::getHead(){
     return head; //getter for head pointer to be used in insertAfter function
 }
-void LinkedList::insert(int data){
-    node* mirror; //creating mirror pointer
+void LinkedList::insert(node* &Head,int data){
     node *newNode=new node; //initializing temporary node pointer
     newNode->value=data;
     newNode->next=NULL;
-    if (head==NULL){ //if list is empty
-        head=newNode; //make head pointer point to the new node
+    if (Head==NULL){ //if list is empty
+        Head=newNode; //make head pointer point to the new node
     }
-    mirror=head; //making mirror pointer point at first node
+    mirror=Head; //making mirror pointer point at first node
     while (mirror->next!=NULL){
         mirror=mirror->next; //increment mirror pointer until it points to the last node
     }
     mirror->next=newNode; //making last node point to new node
-    newNode->next=NULL; //making new node point to NULL
 }
 void LinkedList::removeFromEnd(){
-    node* mirror; //initializing mirror pointer
-    node* mirror2; //initializing another mirror pointer
     mirror=head;
     mirror2=head; //making both pointers point to first node
     while (mirror != NULL){
@@ -43,8 +41,6 @@ void LinkedList::removeFromEnd(){
     mirror2->next=NULL; //making mirror2 point to NULL, deleting the last node of the list
 }
 void LinkedList::removeFromMiddle(int previousNodeValue){
-    node* mirror; //initializing mirror pointer
-    node* mirror2; //initializing another mirror pointer
     mirror=head;
     mirror2=head; //making both pointers point to first node
     while ((mirror->value) != previousNodeValue){
@@ -54,7 +50,6 @@ void LinkedList::removeFromMiddle(int previousNodeValue){
     mirror2->next=mirror->next; //making mirror2 point to node after mirror, deleting the node mirror is pointing to
 }
 void LinkedList::print(){
-    node* mirror; //initializing mirror pointer
     mirror=head; //making mirror point to first node
     cout << "the elements of the linked list are: " << endl;
     while (mirror!=NULL){
@@ -63,7 +58,6 @@ void LinkedList::print(){
     }
 }
 int LinkedList::sum(){
-    node* mirror; //initializing mirror pointer
     mirror=head; //making mirror point to first node
     int sum=0; //initializing sum with 0
     while (mirror!=NULL){
@@ -75,26 +69,19 @@ int LinkedList::sum(){
 LinkedList LinkedList::create(vector<int> v){
     LinkedList list; //initializing new LinkedList object
     
-    vector<int>::iterator iter; //initializing iterator for the vector
-    sort(v.begin(),v.end()); //sorting the vector ascendingly
-    iter=unique(v.begin(), v.end()); //using unique function to remove all duplicates from the vector
-    v.resize(distance(v.begin(), iter)); //resizing the vector to only include the non-duplicate integers
-    for (int i=0;i<v.size();i++)
-        list.insert(v[i]);
-    
-    /*node* mirror=head;
-    node* mirror2=head;
-    
-    for (int i=0;i<v.size();i++)
-        list.insert(v[i]);
-    
-    while (mirror!=NULL){
-        mirror2=mirror;
-        mirror=mirror->next;
-        if (mirror->value == mirror2->value){
-            list.removeFromMiddle(mirror2->value);
-            mirror2->numOfOccurance++;
+    LinkedList list; //initializing new LinkedList object
+    list.insert(head, v[0]); //inserting first vector element in list
+    mirror=head; //making mirror point to head
+        for (int i=1;i<v.size();i++){ //checking if each vector element has duplicates by traversing the whole linked list
+            if (mirror->value==v[i]){
+                mirror->numOfOccurance++;
+            }
+            else{
+                list.insert(head,v[i]);
+            }
+            mirror=mirror->next;
+            
         }
-    }*/
-    return list; //returning the list
+        return list; //returning the list
+
 }
